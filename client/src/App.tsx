ec0a8b515806e64abc9077f81f87783c6debe403
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-12-14 20:57:46 +0100

    front-end packakages install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./components/chat";
+//import Login from './Login';
+//import Register from './Register';
+
 function App() {
   return (
   <div className="app">
-    app
+    <BrowserRouter>
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </div>
-  )
+  );
 }
 
-export default App
+export default App;
